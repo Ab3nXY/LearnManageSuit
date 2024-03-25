a3e48fb4f9b3d7679f7ea72f6c4c7d05a073ab5b
ab3nxy <abenxy0@gmail.com>
2024-03-25 17:26:19 +0300

    Project Url updated

--- eventcalendar/urls.py
+++ eventcalendar/urls.py
@@ -1,21 +1,5 @@
-"""eventcalendar URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, ifdebug
 from .views import DashboardView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,9 +10,10 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("", include("calendarapp.urls")),
     path('accounts/', include('allauth.urls')),
-    path("__debug__/", include("debug_toolbar.urls")),
+    ifdebug():
+        path("__debug__/", include("debug_toolbar.urls")),
 ]
 
-# Display images
+# Serve static and media files
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
